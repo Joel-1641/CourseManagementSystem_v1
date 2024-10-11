90590340080a49e4516dc406b4a493a452e6d370
Joel-1641 <ut01641tic2024@gmail.com>
2024-10-11 14:28:35 +0530

    #Question 4.a. Completed

---
+++ CourseManagementSystem_v1/CourseManager.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CourseManagementSystem_v1
+{
+    internal class CourseManager
+    {
+        public List<Course> courses = new List<Course>();
+
+        public void CreateCourse(string title, string duration, decimal price)
+        {
+
+            var courseObj = new Course((courses.Count + 1), title, duration, price);
+            courses.Add(courseObj);
+
+        }
+
+    }
+}
