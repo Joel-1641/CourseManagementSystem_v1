b8c053d4332bda5f456ccb809930e1d22936352d
Joel-1641 <ut01641tic2024@gmail.com>
2024-10-11 16:57:08 +0530

    #Question 13 Add course Created

---
+++ CourseManagementSystem_v1/CourseRepository.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CourseManagementSystem_v1
+{
+    internal class CourseRepository
+    {
+        private readonly string _connectionString = "Server=(localdb)\\MSSQLLocalDB; database=CourseManagement;Trusted_Connection=true;TrustServerCertificate=true;";
+        public void AddCourse(string title, string duration, decimal price)
+        {
+
+            try
+            {
+                using (var connection = new SqlConnection(_connectionString))
+                {
+                    connection.Open();
+                    var command = connection.CreateCommand();
+                    command.CommandText = @"INSERT INTO Course(Title,Duration,Price) VALUES(@title,@duration,@price);";
+                    command.Parameters.AddWithValue("@title", title);
+                    command.Parameters.AddWithValue("@duration", duration);
+                    command.Parameters.AddWithValue("@price", price);
+                    command.ExecuteNonQuery();
+                };
+                Console.WriteLine("Course Add Successfully..");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"ERROR: {ex.Message}");
+            }
+
+        }
+
+
+    }
+}
