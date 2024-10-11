e85b159f4b9e4c50b1ad38368bedc19a87cccbe1
Joel-1641 <ut01641tic2024@gmail.com>
2024-10-11 14:15:45 +0530

    #Question 2 Completed

---
+++ CourseManagementSystem_v1/Course.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CourseManagementSystem_v1
+{
+    internal class Course
+
+    {
+        private int CourseId { get; set; }
+        private string Title { get; set; }
+        private string Duration { get; set; }
+        private decimal Price { get; set; }
+
+    }
+}
+
