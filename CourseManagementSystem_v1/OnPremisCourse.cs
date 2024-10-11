2a3f7b270250bc18a9d13ac94bca6e54bee3f191
Joel-1641 <ut01641tic2024@gmail.com>
2024-10-11 15:22:50 +0530

    #Question 6.a.(OnPremisCourse) Completed

---
+++ CourseManagementSystem_v1/OnPremisCourse.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CourseManagementSystem_v1
+{
+        internal class OnPremisCourse : Course
+        {
+            public string Schedule { get; set; }
+            public int ClassRoomCapacity { get; set; }
+
+            public OnPremisCourse(int courseId, string title, string duration, decimal price, string schedule, int classRoomCapacity) : base(courseId, title, duration, price)
+            {
+                Schedule = schedule;
+                ClassRoomCapacity = classRoomCapacity;
+            }           
+        }
+    }
+
